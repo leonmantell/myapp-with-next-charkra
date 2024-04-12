3d1089d6a0af06a4ab536d27bbb5754193302a50
leonmantell <piedraj.hs@gmail.com>
2024-04-12 16:58:32 +0900

    added a welcome page

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <Text fontSize="100px" color="tomato">
+      Welcome!!
+    </Text>
+  );
+};
+
+export default App;
